3c129fcd145f180978bab1f62a74e9423a473e57
Jelgar <jonathanelgardiner@gmail.com>
2016-03-24 14:53:39 +0000

    teams have a user_id

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,20 @@
 class TeamsController < ApplicationController
 
   def index
+    @teams = Team.all
   end
 
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.create(team_params.merge({:user_id => current_user.id}))
+    redirect_to '/teams'
+  end
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
 
 end
--- spec/features/team_feature_spec.rb
+++ spec/features/team_feature_spec.rb
@@ -1,11 +1,44 @@
 require 'rails_helper'
+context "user signed in on the homepage" do
+  before do
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'test@example.com')
+    fill_in('Password', with: 'testtest')
+    fill_in('Password confirmation', with: 'testtest')
+    click_button('Sign up')
+  end
 
-feature 'teams' do
-  context 'no teams have been' do
-    scenario 'should display a prompt to add a team' do
+  feature 'teams' do
+    context 'no teams have been' do
+      scenario 'should display a prompt to add a team' do
+        visit '/teams'
+        expect(page).to have_content 'No teams yet'
+        expect(page).to have_link 'Add a team'
+      end
+    end
+  end
+
+  context 'Teams have been added' do
+    before do
+      Team.create(name: 'Kings XI')
+    end
+
+    scenario 'display teams' do
       visit '/teams'
-      expect(page).to have_content 'No teams yet'
-      expect(page).to have_link 'Add a team'
+      expect(page).to have_content('Kings XI')
+      expect(page).not_to have_content('No teams yet')
+    end
+  end
+
+  context 'creating teams' do
+    scenario 'prompts user to fill out a form, then displays the new team' do
+      visit '/teams'
+      click_link 'Add a team'
+      fill_in 'Name', with: 'Kings XI'
+      click_button 'Create Team'
+      expect(page).to have_content 'Kings XI'
+      expect(current_path).to eq '/teams'
     end
   end
 end
